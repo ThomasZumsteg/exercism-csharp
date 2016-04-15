b15b42928a0d18aaa397a85fd65924a2b2745772
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-15 12:56:26 -0700

    First iteration bob

---
+++ bob/Bob.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+public class Bob {
+	public static string Hey(string greeting) {
+		greeting = greeting.Trim ();
+		if (greeting == "") {
+			return "Fine. Be that way!";
+		} else if (Regex.IsMatch(greeting, "[A-Z]") &&
+			!Regex.IsMatch(greeting, "[a-z]")) {
+			return "Whoa, chill out!";
+		} else if (greeting.Substring (greeting.Length - 1) == "?") {
+			return "Sure.";
+		}
+		return "Whatever.";
+	}
+}
