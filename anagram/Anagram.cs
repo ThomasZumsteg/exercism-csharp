11a1bc6e5072927289260f212b3e8b283a203f6c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-17 12:35:46 -0700

    First iteration anagram

---
+++ anagram/Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Anagram {
+
+	private char[] letters;
+	private string word;
+
+	/* Anagram finds words that have the same letters but are not the same word */
+	public Anagram(string word) {
+		this.word = word.ToLower ();
+		letters = word.ToLower ().ToCharArray();
+		Array.Sort (letters);
+	}
+
+	/* Match selects words from a list of words that have the same letters
+	 * as the anagram but are not the same word */
+	public string[] Match(string[] words) {
+		var matches = new List<string>(); 
+		foreach(string word in words) {
+			var letters = word.ToLower ().ToCharArray();
+			Array.Sort (letters);
+			if (this.letters == letters && this.word != word.ToLower()) {
+				matches.Add(word);
+			}
+		}
+		return matches.ToArray();
+	}
+}
+
