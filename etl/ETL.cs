ed4fea035cc89a0e6591511b1c2b546fe19f5c4b
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-16 19:28:12 -0700

    First iteration ETL

---
+++ etl/ETL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using oldFormat = System.Collections.Generic.Dictionary<int, 
+	System.Collections.Generic.IList<string>>;
+using newFormat = System.Collections.Generic.Dictionary<string, int>;
+
+public class ETL {
+	public static newFormat Transform(oldFormat old) {
+		newFormat new_dict = new newFormat();
+		foreach( KeyValuePair<int, IList<string>> entry in old) {
+			foreach( string val in entry.Value ) {
+				new_dict[val.ToLower()] = entry.Key;
+			}
+		}
+
+		return new_dict;
+	}
+}
