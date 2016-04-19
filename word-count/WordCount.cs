d4e0f0a8dbc152548e23f30c854391354e0f79b1
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-19 10:09:22 -0700

    First iteration word-count

---
+++ word-count/WordCount.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using WordDict = System.Collections.Generic.Dictionary<string, int>;
+
+public class Phrase {
+
+	/* WordCount counts the number of times words appear in a sentance */
+	public static WordDict WordCount(string words) {
+		var counter = new WordDict (); 
+		words = Regex.Replace (words.ToLower (), "[^a-z]", string.Empty);
+		foreach(var word in words.Split(new char[] {' '})) {
+			if (!counter.ContainsKey (word))
+				counter [word] = 0;
+			counter [word]++;
+		}
+		return counter;
+	}
+
+}
