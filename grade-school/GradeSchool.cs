a8175f357061b9caafb8dee15ef87d9c900b013c
Thomas Zumsteg <thomas.zumsteg@gmail.com>
2016-04-20 10:27:31 -0700

    First iteration grade school

---
+++ grade-school/GradeSchool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class School {
+	public Dictionary<int, List<string>> Roster = new Dictionary<int, List<string>>{};
+
+	public School() {
+	}
+
+	public void Add(string student, int grade) {
+		if( !Roster.ContainsKey(grade) )
+			Roster[grade] = new List<string>();
+		Roster[grade].Add (student);
+		Roster[grade].Sort();
+	}
+
+	public List<string> Grade(int grade) {
+		if (!Roster.ContainsKey (grade))
+			return new List<string>();
+		return Roster [grade];
+	}
+
+
+}
